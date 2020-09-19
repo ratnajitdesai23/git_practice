a601f9739cd301db3fc9b084cd7233808b2d7986
Ratnajit <Ratnajitdesai@gmail.com>
2020-09-20 00:41:15 +0530

    first commit

---
+++ ProblemSolver.java
@@ -0,0 +1,10 @@
+Class ProblemSolver{
+
+	public static void main(String[] args)
+	{
+
+		System.out.println("Calling.....ProblemSolver");
+		System.out.println("Calling.....ProblemSolver - 2");
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Install JDK on machine
